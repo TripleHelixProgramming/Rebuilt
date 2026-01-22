2f8f57d8a90e5b9667e86953ee4cd6498147c459
Nate Laverdure <41549690+nlaverdure@users.noreply.github.com>
2026-01-22 18:59:10 -0500

    Turret (#24)
    * added turret controls
    
    * added turret subsystem
    
    * spelling
    
    * add periodic and control methods
    
    * add default command
    
    * Adjusted setOrientation command to get it to point at the hub correctly
    
    * include chassis-to-turret transform2d
    
    * reorganization
    
    * add simulation
    
    * fix runtime errors
    
    * add feedforward
    
    * account for nonzero tangential velocity in FF
    
    * add IsOnTarget
    
    * turret is aimed when pointing within 1 radus of hub
    
    * use speeds at the turret center rather than chassis center
    
    * added log in drive
    
    * use field relative chassis speeds
    
    * construct field relative speeds from robot relative speeds
    
    ---------
    
    Co-authored-by: BenGamer3 <benbenw2020@gmail.com>
    Co-authored-by: DylanTaylor29 <132412179+DylanTaylor29@users.noreply.github.com>

---
+++ src/main/java/frc/robot/subsystems/turret/Turret.java
@@ -0,0 +1,104 @@
+package frc.robot.subsystems.turret;
+
+import static edu.wpi.first.units.Units.*;
+import static frc.robot.subsystems.turret.TurretConstants.*;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.Alert;
+import edu.wpi.first.wpilibj.Alert.AlertType;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.FieldConstants;
+import frc.robot.Constants.RobotConstants;
+import java.util.function.Supplier;
+import org.littletonrobotics.junction.AutoLogOutput;
+import org.littletonrobotics.junction.Logger;
+
+public class Turret extends SubsystemBase {
+  private final TurretIO io;
+  private final TurretIOInputsAutoLogged inputs = new TurretIOInputsAutoLogged();
+  private final Supplier<Pose2d> chassisPoseSupplier;
+  private final Supplier<ChassisSpeeds> chassisSpeedsSupplier;
+
+  private final Alert turnDisconnectedAlert;
+
+  private Rotation2d turretOrientationSetpoint = Rotation2d.kZero;
+  private Translation2d hubToTurretBase = new Translation2d();
+
+  public Turret(
+      Supplier<Pose2d> chassisPoseSupplier,
+      Supplier<ChassisSpeeds> chassisSpeedsSupplier,
+      TurretIO io) {
+    this.io = io;
+    this.chassisPoseSupplier = chassisPoseSupplier;
+    this.chassisSpeedsSupplier = chassisSpeedsSupplier;
+    turnDisconnectedAlert = new Alert("Disconnected turret turn motor", AlertType.kError);
+  }
+
+  @Override
+  public void periodic() {
+    io.updateInputs(inputs);
+    Logger.processInputs("Turret", inputs);
+    turnDisconnectedAlert.set(!inputs.turnConnected);
+  }
+
+  public void stop() {
+    io.setTurnOpenLoop(0.0);
+  }
+
+  public void aimAtHub() {
+    var hub = FieldConstants.kBlueHubCenter; // TODO: Point at the hub of the correct alliance color
+    var turretBase = chassisPoseSupplier.get().plus(chassisToTurretBase);
+    hubToTurretBase = turretBase.getTranslation().minus(hub);
+
+    turretOrientationSetpoint =
+        hubToTurretBase.unaryMinus().getAngle().minus(turretBase.getRotation());
+
+    var robotRelative = chassisSpeedsSupplier.get();
+    var fieldRelative =
+        ChassisSpeeds.fromRobotRelativeSpeeds(robotRelative, turretBase.getRotation());
+    Translation2d turretBaseSpeeds = getTurretBaseSpeeds(fieldRelative);
+    var tangentialSpeed =
+        turretBaseSpeeds.cross(hubToTurretBase)
+            / hubToTurretBase.getNorm()
+            / hubToTurretBase.getNorm();
+
+    double feedforwardVolts =
+        RobotConstants.kNominalVoltage
+            * -(2 * fieldRelative.omegaRadiansPerSecond
+                + tangentialSpeed) // TODO: Why is this factor 2 needed?
+            / turnMaxAngularVelocity.in(RadiansPerSecond);
+
+    io.setTurnPosition(turretOrientationSetpoint, feedforwardVolts);
+  }
+
+  @AutoLogOutput(key = "Turret/Pose")
+  public Pose2d getTurretPose() {
+    return chassisPoseSupplier
+        .get()
+        .plus(chassisToTurretBase)
+        .plus(new Transform2d(0, 0, inputs.turnPosition));
+  }
+
+  @AutoLogOutput(key = "Turret/IsOnTarget")
+  public boolean isOnTarget() {
+    return inputs.turnPosition.minus(turretOrientationSetpoint).getMeasure().abs(Radians)
+            * hubToTurretBase.getNorm()
+        < (hubWidth.in(Meters) / 2.0);
+  }
+
+  private Translation2d getTurretBaseSpeeds(ChassisSpeeds chassisSpeeds) {
+    // Extract translation from the transform
+    Translation2d r = chassisToTurretBase.getTranslation();
+
+    double vx = chassisSpeeds.vxMetersPerSecond;
+    double vy = chassisSpeeds.vyMetersPerSecond;
+    double omega = chassisSpeeds.omegaRadiansPerSecond;
+
+    // Rigid-body velocity equation
+    return new Translation2d(vx - omega * r.getY(), vy + omega * r.getX());
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/turret/TurretIOSpark.java
@@ -0,0 +1,107 @@
+package frc.robot.subsystems.turret;
+
+import static frc.robot.subsystems.turret.TurretConstants.*;
+import static frc.robot.util.SparkUtil.*;
+
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.FeedbackSensor;
+import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.math.geometry.Rotation2d;
+import frc.robot.Constants.MotorConstants.NEO550Constants;
+import frc.robot.Constants.RobotConstants;
+import java.util.function.DoubleSupplier;
+
+public class TurretIOSpark implements TurretIO {
+
+  private final SparkBase turnSpark;
+  private final AbsoluteEncoder turnEncoder;
+  private final SparkClosedLoopController turnController;
+  private final Debouncer turnConnectedDebounce =
+      new Debouncer(0.5, Debouncer.DebounceType.kFalling);
+
+  public TurretIOSpark() {
+    turnSpark = new SparkMax(port, MotorType.kBrushless);
+    turnEncoder = turnSpark.getAbsoluteEncoder();
+    turnController = turnSpark.getClosedLoopController();
+
+    var turnConfig = new SparkMaxConfig();
+
+    turnConfig
+        .inverted(false)
+        .idleMode(IdleMode.kBrake)
+        .smartCurrentLimit(NEO550Constants.kDefaultSupplyCurrentLimit)
+        .voltageCompensation(RobotConstants.kNominalVoltage);
+
+    turnConfig
+        .absoluteEncoder
+        .inverted(false)
+        .positionConversionFactor(turnEncoderPositionFactor)
+        .velocityConversionFactor(turnEncoderVelocityFactor)
+        .averageDepth(2);
+
+    turnConfig
+        .closedLoop
+        .feedbackSensor(FeedbackSensor.kAbsoluteEncoder)
+        .positionWrappingEnabled(true)
+        .positionWrappingInputRange(turnPIDMinInput, turnPIDMaxInput)
+        .pid(turnKp, 0.0, 0.0);
+
+    turnConfig
+        .signals
+        .absoluteEncoderPositionAlwaysOn(true)
+        .absoluteEncoderPositionPeriodMs(20)
+        .absoluteEncoderVelocityAlwaysOn(true)
+        .absoluteEncoderVelocityPeriodMs(20)
+        .appliedOutputPeriodMs(20)
+        .busVoltagePeriodMs(20)
+        .outputCurrentPeriodMs(20);
+
+    tryUntilOk(
+        turnSpark,
+        5,
+        () ->
+            turnSpark.configure(
+                turnConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters));
+  }
+
+  @Override
+  public void updateInputs(TurretIOInputs inputs) {
+    sparkStickyFault = false;
+    ifOk(
+        turnSpark,
+        turnEncoder::getPosition,
+        (value) -> inputs.turnPosition = new Rotation2d(value).minus(rotationOffset));
+    ifOk(turnSpark, turnEncoder::getVelocity, (value) -> inputs.turnVelocityRadPerSec = value);
+    ifOk(
+        turnSpark,
+        new DoubleSupplier[] {turnSpark::getAppliedOutput, turnSpark::getBusVoltage},
+        (values) -> inputs.turnAppliedVolts = values[0] * values[1]);
+    ifOk(turnSpark, turnSpark::getOutputCurrent, (value) -> inputs.turnCurrentAmps = value);
+    inputs.turnConnected = turnConnectedDebounce.calculate(!sparkStickyFault);
+  }
+
+  @Override
+  public void setTurnOpenLoop(double output) {
+    turnSpark.setVoltage(output);
+  }
+
+  @Override
+  public void setTurnPosition(Rotation2d rotation, double feedforwardVolts) {
+    double setpoint =
+        MathUtil.inputModulus(
+            rotation.plus(rotationOffset).getRadians(), turnPIDMinInput, turnPIDMaxInput);
+    turnController.setSetpoint(
+        setpoint, ControlType.kPosition, ClosedLoopSlot.kSlot0, feedforwardVolts);
+  }
+}
