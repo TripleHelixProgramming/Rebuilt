93469ee96135151532661a1617ff25ce34eaec86
Nate Laverdure <nate.laverdure@gmail.com>
2026-01-25 10:02:06 -0500

    wip use wpilib geometry classes

--- src/main/java/frc/game/Field.java
+++ src/main/java/frc/game/Field.java
@@ -1,100 +1,59 @@
 package frc.game;
 
-import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.*;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rectangle2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.units.measure.Distance;
 
 public class Field {
-  public static final double FIELD_WIDTH = 317.7;
-  public static final double ALLIANCE_ZONE_LENGTH = 158.6;
-  public static final double NEUTRAL_ZONE_LENGTH = 283;
+  public static final Distance centerField_x_pos = Inches.of(325.06);
+  public static final Distance centerField_y_pos = Inches.of(316.64);
+  private static final Distance hub_x_pos = Inches.of(181.56);
+  private static final Distance hub_x_len = Inches.of(47);
+  private static final Distance hub_y_len = Inches.of(47);
+  private static final Distance trench_y_len = Inches.of(49.86);
+
+  private static final Distance field_x_len = centerField_x_pos.times(2);
+  private static final Distance field_y_len = centerField_y_pos.times(2);
+  private static final Distance allianceZone_x_len = hub_x_pos.minus(hub_x_len.div(2));
+  private static final Distance neutralZone_x_len = centerField_x_pos.minus(hub_x_pos.plus(hub_x_len)).times(2);
+  private static final Pose2d fieldCenter = new Pose2d(new Translation2d(centerField_x_pos, centerField_y_pos), Rotation2d.kZero);
+  private static final Pose2d blueHubCenter = new Pose2d(new Translation2d(hub_x_pos, centerField_y_pos), Rotation2d.kZero);
+  private static final Pose2d redHubCenter = new Pose2d(new Translation2d(centerField_x_pos.times(2).minus(hub_x_pos), centerField_y_pos), Rotation2d.kZero);
 
   enum Region {
-    BlueZone(build().xpos(0).ypos(0).xlen(ALLIANCE_ZONE_LENGTH).ylen(FIELD_WIDTH)),
-    NeutralZone(
-        build().xpos(ALLIANCE_ZONE_LENGTH).ypos(0).xlen(NEUTRAL_ZONE_LENGTH).ylen(FIELD_WIDTH)),
-    RedZone(
-        build()
-            .xpos(ALLIANCE_ZONE_LENGTH + NEUTRAL_ZONE_LENGTH)
-            .ypos(0)
-            .xlen(ALLIANCE_ZONE_LENGTH)
-            .ylen(FIELD_WIDTH)),
-    RedLeftTrench(
-        build()
-            .xpos(ALLIANCE_ZONE_LENGTH + NEUTRAL_ZONE_LENGTH)
-            .ypos(FIELD_WIDTH)
-            .xlen(65.65)
-            .ylen(47)),
-    RedRightTrench(
-        build().xpos(ALLIANCE_ZONE_LENGTH + NEUTRAL_ZONE_LENGTH).ypos(0).xlen(65.65).ylen(47)),
-    BlueLeftTrench(build().xpos(ALLIANCE_ZONE_LENGTH).ypos(FIELD_WIDTH).xlen(65.65).ylen(47)),
-    BlueRightTrench(build().xpos(ALLIANCE_ZONE_LENGTH).ypos(0).xlen(65.65).ylen(47)),
-    BlueDepot(build().xpos(ALLIANCE_ZONE_LENGTH).ypos(65.65).xlen(42).ylen(27)),
-    RedDepot(build().xpos(FIELD_WIDTH).ypos(91.4 + 73).xlen(42).ylen(27)),
-    BlueTower(build().xpos(ALLIANCE_ZONE_LENGTH).ypos(91.4).xlen(49.25).ylen(45)),
-    RedTower(build().xpos(FIELD_WIDTH).ypos(91.4).xlen(49.25).ylen(45)),
-    BlueHub(build().xpos(ALLIANCE_ZONE_LENGTH).ypos(138.65).xlen(47).ylen(47)),
-    RedHub(build().xpos(ALLIANCE_ZONE_LENGTH + NEUTRAL_ZONE_LENGTH).ypos(138.65).xlen(47).ylen(47)),
+    BlueZone(new Rectangle2d(new Translation2d(0, 0), new Translation2d(allianceZone_x_len, field_y_len))),
+    NeutralZone(new Rectangle2d(fieldCenter, neutralZone_x_len, field_y_len)),
+    RedZone(new Rectangle2d(new Translation2d(field_x_len.minus(allianceZone_x_len), Inches.of(0)), new Translation2d(field_x_len, field_y_len))),
+    BlueHub(new Rectangle2d(blueHubCenter, hub_x_len, hub_y_len)),
+    RedHub(new Rectangle2d(redHubCenter, hub_x_len, hub_y_len)),
+    RedLeftTrench(new Rectangle2d(new Translation2d(field_x_len.minus(hub_x_pos).minus(hub_x_len.div(2)), Inches.of(0)), new Translation2d(field_x_len.minus(hub_x_pos).plus(hub_x_len.div(2)), trench_y_len))),
+    RedRightTrench(new Rectangle2d(new Translation2d(field_x_len.minus(hub_x_pos).minus(hub_x_len.div(2)), field_y_len.minus(trench_y_len)), new Translation2d(field_x_len.minus(hub_x_pos).plus(hub_x_len.div(2)), field_y_len))),
+    BlueLeftTrench(new Rectangle2d(new Translation2d(hub_x_pos.minus(hub_x_len.div(2)), field_y_len.minus(trench_y_len)), new Translation2d(hub_x_pos.plus(hub_x_len.div(2)), field_y_len))),
+    BlueRightTrench(new Rectangle2d(new Translation2d(hub_x_pos.minus(hub_x_len.div(2)), Inches.of(0)), new Translation2d(hub_x_pos.plus(hub_x_len.div(2)), trench_y_len))),
+    BlueDepot(build().xpos(0).ypos(centerField_y_pos + 75.93 - 42/2).xlen(27).ylen(42)),
+    RedDepot(build().xpos(2*centerField_x_pos - 27).ypos(centerField_y_pos - 75.93 - 42/2).xlen(27).ylen(42)),
+    // BlueTower(build().xpos(ALLIANCE_ZONE_LENGTH).ypos(91.4).xlen(49.25).ylen(45)),
+    // RedTower(build().xpos(FIELD_WIDTH).ypos(91.4).xlen(49.25).ylen(45)),
     RedLeftBump(
-        build().xpos(ALLIANCE_ZONE_LENGTH + NEUTRAL_ZONE_LENGTH).ypos(135.8).xlen(73).ylen(44.4)),
+        build().xpos(allianceZone_x_len + 47 + neutralZone_x_len).ypos(49.86 + 12 + 73 + 47).xlen(47).ylen(73)),
     RedRightBump(
-        build().xpos(ALLIANCE_ZONE_LENGTH + NEUTRAL_ZONE_LENGTH).ypos(47).xlen(73).ylen(44.4)),
-    BlueLeftBump(build().xpos(ALLIANCE_ZONE_LENGTH).ypos(135.8).xlen(73).ylen(44.4)),
-    BlueRightBump(build().xpos(ALLIANCE_ZONE_LENGTH).ypos(47).xlen(73).ylen(44.4)),
-    FuelArrangement(
-        build().xpos(NEUTRAL_ZONE_LENGTH / 2 + 72 / 2).ypos(317.7 - 47 * 2).xlen(72).ylen(206));
+        build().xpos(allianceZone_x_len + 47 + neutralZone_x_len).ypos(49.86 + 12).xlen(47).ylen(73)),
+    BlueLeftBump(build().xpos(allianceZone_x_len).ypos(49.86 + 12 + 73 + 47).xlen(47).ylen(73)),
+    BlueRightBump(build().xpos(allianceZone_x_len).ypos(49.86 + 12).xlen(47).ylen(73)),
+    FuelArrangement(new Rectangle2d(fieldCenter, Inches.of(35.95*2), Inches.of(90.95*2)));
 
-    public final double xpos;
-    public final double ypos;
-    public final double xlen;
-    public final double ylen;
-    public final Translation2d center;
+    public final Rectangle2d rect;
 
-    private Region(Builder builder) {
-      this.xpos = builder.xpos;
-      this.ypos = builder.ypos;
-      this.xlen = builder.xlen;
-      this.ylen = builder.ylen;
-      this.center = new Translation2d(Inches.of(xpos + xlen / 2), Inches.of(ypos + ylen / 2));
-    }
-
-    public static Builder build() {
-      return new Builder();
-    }
-
-    public static class Builder {
-      double xpos;
-      double ypos;
-      double xlen;
-      double ylen;
-
-      public Builder xpos(double xpos) {
-        this.xpos = xpos;
-        return this;
-      }
-
-      public Builder ypos(double ypos) {
-        this.ypos = ypos;
-        return this;
-      }
-
-      public Builder xlen(double xlen) {
-        this.xlen = xlen;
-        return this;
-      }
-
-      public Builder ylen(double ylen) {
-        this.ylen = ylen;
-        return this;
-      }
+    private Region(Rectangle2d rect) {
+      this.rect = rect;
     }
 
     public boolean contains(Pose2d pose) {
-      return (pose.getX() >= xpos)
-          && (pose.getX() < xpos + xlen)
-          && (pose.getY() >= ypos)
-          && (pose.getY() < ypos + ylen);
+      return rect.contains(pose.getTranslation());
     }
   }
 
