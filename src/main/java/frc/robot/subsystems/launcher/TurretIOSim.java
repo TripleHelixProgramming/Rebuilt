698d94dc1dcf5d28278d8f3019f6ff0a7699533b
BenGamer3 <benbenw2020@gmail.com>
2026-02-07 14:34:26 -0500

    changed turretiosim

--- src/main/java/frc/robot/subsystems/launcher/TurretIOSim.java
+++ src/main/java/frc/robot/subsystems/launcher/TurretIOSim.java
@@ -3,31 +3,80 @@ package frc.robot.subsystems.launcher;
 import static edu.wpi.first.units.Units.RadiansPerSecond;
 import static frc.robot.subsystems.launcher.LauncherConstants.TurretConstants.*;
 
+import com.revrobotics.PersistMode;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.ResetMode;
+import com.revrobotics.sim.SparkMaxSim;
+import com.revrobotics.spark.FeedbackSensor;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+import frc.robot.Constants.MotorConstants.NEO550Constants;
 import frc.robot.Constants.RobotConstants;
 import frc.robot.Robot;
 
 public class TurretIOSim implements TurretIO {
+
   private final DCMotorSim turnSim;
 
+  private final SparkMax turnSpark;
+  private final RelativeEncoder turnSparkEncoder;
+  private final PIDController controller;
+  private final DutyCycleEncoder absoluteEncoder;
   private boolean closedLoop = false;
   private PIDController positionController = new PIDController(kPSim, 0.0, kDSim);
   private double appliedVolts = 0.0;
   private double feedforwardVolts = 0.0;
+  private boolean seeded = false;
+  private final SparkMaxSim turnSparkSim;
 
   public TurretIOSim() {
-    turnSim =
-        new DCMotorSim(LinearSystemId.createDCMotorSystem(gearbox, 0.004, motorReduction), gearbox);
 
     // Enable wrapping for turn PID
     // positionController.enableContinuousInput(-Math.PI, Math.PI);
 
-    turnSim.setAngle(-mechanismOffset.getRadians());
+    turnSpark = new SparkMax(port, MotorType.kBrushless);
+    turnSparkEncoder = turnSpark.getEncoder();
+    controller = new PIDController(kPReal, 0.0, 0.0);
+    absoluteEncoder =
+        new DutyCycleEncoder(
+            new DigitalInput(absEncoderPort),
+            2 * Math.PI,
+            absEncoderOffset.getRadians() + mechanismOffset.getRadians());
+
+    var turnConfig = new SparkMaxConfig();
+
+    turnConfig
+        .inverted(false)
+        .idleMode(IdleMode.kBrake)
+        .smartCurrentLimit(NEO550Constants.kDefaultSupplyCurrentLimit)
+        .voltageCompensation(RobotConstants.kNominalVoltage);
+
+    turnConfig
+        .encoder
+        .positionConversionFactor(encoderPositionFactor)
+        .velocityConversionFactor(encoderVelocityFactor);
+
+    turnConfig.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder).pid(kPReal, 0.0, 0.0);
+
+    turnConfig.signals.appliedOutputPeriodMs(20).busVoltagePeriodMs(20).outputCurrentPeriodMs(20);
+
+    turnSpark.configure(turnConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    turnSparkSim = new SparkMaxSim(turnSpark, gearbox);
+
+    turnSparkSim.setPosition(-mechanismOffset.getRadians());
+
+    turnSim =
+        new DCMotorSim(LinearSystemId.createDCMotorSystem(gearbox, 0.004, motorReduction), gearbox);
   }
 
   @Override
