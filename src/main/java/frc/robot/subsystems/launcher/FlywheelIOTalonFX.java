8355d40482d1eb03b9e7e1a129473d8f7bcbfe69
DylanTaylor29 <132412179+DylanTaylor29@users.noreply.github.com>
2026-02-07 11:36:44 -0500

    finished building out FlywheelIOTalonFX.java

--- src/main/java/frc/robot/subsystems/launcher/FlywheelIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/launcher/FlywheelIOTalonFX.java
@@ -7,66 +7,54 @@ import static frc.robot.util.PhoenixUtil.tryUntilOk;
 import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.PositionTorqueCurrentFOC;
-import com.ctre.phoenix6.controls.PositionVoltage;
-import com.ctre.phoenix6.controls.TorqueCurrentFOC;
-import com.ctre.phoenix6.controls.VelocityTorqueCurrentFOC;
+import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.math.filter.Debouncer;
-import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Current;
 import edu.wpi.first.units.measure.Voltage;
-import frc.robot.subsystems.drive.PhoenixOdometryThread;
-import java.util.Queue;
 
 public class FlywheelIOTalonFX implements FlywheelIO {
-  private final TalonFX flywheelTalon;
+  private final TalonFX flywheelLeaderTalon;
+  private final TalonFX flywheelFollowerTalon;
   private final TalonFXConfiguration config;
-  private final Debouncer turnConnectedDebounce =
+  private final Debouncer flywheelConnectedDebounce =
       new Debouncer(0.5, Debouncer.DebounceType.kFalling);
 
   // Voltage control requests
   private final VoltageOut voltageRequest = new VoltageOut(0);
-  private final PositionVoltage positionVoltageRequest = new PositionVoltage(0.0);
   private final VelocityVoltage velocityVoltageRequest = new VelocityVoltage(0.0);
 
   // Torque-current control requests
-  private final TorqueCurrentFOC torqueCurrentRequest = new TorqueCurrentFOC(0);
-  private final PositionTorqueCurrentFOC positionTorqueCurrentRequest =
-      new PositionTorqueCurrentFOC(0.0);
-  private final VelocityTorqueCurrentFOC velocityTorqueCurrentRequest =
-      new VelocityTorqueCurrentFOC(0.0);
+  //   private final TorqueCurrentFOC torqueCurrentRequest = new TorqueCurrentFOC(0);
+  //   private final VelocityTorqueCurrentFOC velocityTorqueCurrentRequest =
+  //       new VelocityTorqueCurrentFOC(0.0);
 
-  // Timestamp inputs from Phoenix thread
-  private final Queue<Double> timestampQueue;
-
-  // Inputs from drive motor
-  private final StatusSignal<Angle> flywheelPosition;
-  private final Queue<Double> flywheelPositionQueue;
+  // Inputs from flywheel motor
   private final StatusSignal<AngularVelocity> flywheelVelocity;
   private final StatusSignal<Voltage> flywheelAppliedVolts;
   private final StatusSignal<Current> flywheelCurrent;
 
   public FlywheelIOTalonFX() {
-    flywheelTalon = new TalonFX(port, kCANBus);
+    flywheelLeaderTalon = new TalonFX(leaderPort, kCANBus);
+    flywheelFollowerTalon = new TalonFX(followerPort, kCANBus);
     // Configuration
     config = new TalonFXConfiguration();
-    config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+    config.MotorOutput.withInverted(InvertedValue.CounterClockwise_Positive)
+        .withNeutralMode(NeutralModeValue.Brake);
     config.Slot0 = flywheelGains;
-    tryUntilOk(5, () -> flywheelTalon.getConfigurator().apply(config, 0.25));
+    tryUntilOk(5, () -> flywheelLeaderTalon.getConfigurator().apply(config, 0.25));
+    tryUntilOk(5, () -> flywheelFollowerTalon.getConfigurator().apply(config, 0.25));
 
-    timestampQueue = PhoenixOdometryThread.getInstance().makeTimestampQueue();
-
-    flywheelPosition = flywheelTalon.getPosition();
-    flywheelPositionQueue =
-        PhoenixOdometryThread.getInstance().registerSignal(flywheelPosition.clone());
-    flywheelVelocity = flywheelTalon.getVelocity();
-    flywheelAppliedVolts = flywheelTalon.getMotorVoltage();
-    flywheelCurrent = flywheelTalon.getStatorCurrent();
+    flywheelVelocity = flywheelLeaderTalon.getVelocity();
+    flywheelAppliedVolts = flywheelLeaderTalon.getMotorVoltage();
+    flywheelCurrent = flywheelLeaderTalon.getStatorCurrent();
 
     BaseStatusSignal.setUpdateFrequencyForAll(
         50.0,
@@ -76,5 +64,39 @@ public class FlywheelIOTalonFX implements FlywheelIO {
         flywheelVelocity,
         flywheelAppliedVolts,
         flywheelCurrent);
+
+    flywheelFollowerTalon.setControl(new Follower(leaderPort, MotorAlignmentValue.Opposed));
+  }
+
+  @Override
+  public void updateInputs(FlywheelIOInputs inputs) {
+    BaseStatusSignal.refreshAll(
+            flywheelVelocity,
+            flywheelAppliedVolts,
+            flywheelCurrent,
+            flywheelVelocity,
+            flywheelAppliedVolts,
+            flywheelCurrent)
+        .isOK();
+
+    inputs.appliedVolts = flywheelAppliedVolts.getValueAsDouble();
+    inputs.connected =
+        flywheelConnectedDebounce.calculate(
+            flywheelAppliedVolts.getStatus().isOK()
+                && flywheelCurrent.getStatus().isOK()
+                && flywheelVelocity.getStatus().isOK());
+    inputs.currentAmps = flywheelCurrent.getValueAsDouble();
+    inputs.velocityRadPerSec =
+        Units.rotationsToRadians(flywheelVelocity.getValueAsDouble()) / gearboxRatio;
+  }
+
+  @Override
+  public void setOpenLoop(double output) {
+    flywheelLeaderTalon.setControl(voltageRequest.withOutput(output));
+  }
+
+  @Override
+  public void setVelocity(AngularVelocity angularVelocity) {
+    flywheelLeaderTalon.setControl(velocityVoltageRequest.withVelocity(angularVelocity));
   }
 }
