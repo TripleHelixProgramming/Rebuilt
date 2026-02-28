5598ff1b6e9cbe0144507c422fc972133fa3488d
Nate Laverdure <41549690+nlaverdure@users.noreply.github.com>
2026-02-28 17:03:28 -0500

    Update turret (#112)
    * update turret configuration
    
    * created simulated drive and vision systems for testing the turret
    
    * update encoder offset
    
    * fixed logging issues with refreshing the signals
    
    * added back in real vision and drivetrain
    
    * expand perf envelope to 180
    
    ---------
    
    Co-authored-by: DylanTaylor29 <132412179+DylanTaylor29@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/launcher/LauncherConstants.java
+++ src/main/java/frc/robot/subsystems/launcher/LauncherConstants.java
@@ -69,9 +69,9 @@ public final class LauncherConstants {
     // Geometry
     public static final Transform3d chassisToTurretBase =
         new Transform3d(Inches.of(-2.271), Inches.of(-4.959), Inches.of(16.331), Rotation3d.kZero);
-    public static final Rotation2d absEncoderOffset = new Rotation2d(2.04);
-    public static final Rotation2d mechanismOffset = Rotation2d.k180deg;
-    public static final Angle rangeOfMotion = Degrees.of(5);
+    public static final Rotation2d absEncoderOffset = new Rotation2d(5.157);
+    public static final Rotation2d mechanismOffset = Rotation2d.kZero;
+    public static final Angle rangeOfMotion = Degrees.of(180);
 
     // Position controller
     public static final double kPReal = 0.5;
