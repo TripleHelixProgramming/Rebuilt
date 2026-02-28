c34bd1977e19c4f835985015a1f23cffce8a4b02
nlaverdure <nate.laverdure@gmail.com>
2026-02-28 17:09:11 -0500

    fix error in conflict resolution

--- src/main/java/frc/robot/subsystems/launcher/LauncherConstants.java
+++ src/main/java/frc/robot/subsystems/launcher/LauncherConstants.java
@@ -72,6 +72,7 @@ public final class LauncherConstants {
     public static final Rotation2d absEncoderOffset = new Rotation2d(5.157);
     public static final Rotation2d mechanismOffset = Rotation2d.kZero;
     public static final Angle rangeOfMotion = Degrees.of(180);
+    public static final Angle margin = Degrees.of(5);
 
     // Position controller
     public static final double kPReal = 0.5;
