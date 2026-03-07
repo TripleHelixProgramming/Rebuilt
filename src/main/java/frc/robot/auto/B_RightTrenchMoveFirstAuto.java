144589c16a363e73f141db996ce78b6952264b6a
Dylan <132412179+DylanTaylor29@users.noreply.github.com>
2026-03-06 22:22:39 -0500

    created new move first autos (#138)

---
+++ src/main/java/frc/robot/auto/B_RightTrenchMoveFirstAuto.java
@@ -0,0 +1,75 @@
+package frc.robot.auto;
+
+import choreo.auto.AutoRoutine;
+import choreo.auto.AutoTrajectory;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.drive.Drive;
+import frc.robot.subsystems.feeder.Feeder;
+import frc.robot.subsystems.intake.Intake;
+import frc.robot.subsystems.launcher.Launcher;
+
+public class B_RightTrenchMoveFirstAuto extends AutoMode {
+  Drive drive;
+  Feeder feeder;
+  Intake intake;
+  Launcher launcher;
+
+  public B_RightTrenchMoveFirstAuto(
+      Drive drivetrain,
+      Feeder feederSubsystem,
+      Intake intakeSubsystem,
+      Launcher launcherSubsystem) {
+    super(drivetrain);
+    drive = drivetrain;
+    feeder = feederSubsystem;
+    intake = intakeSubsystem;
+    launcher = launcherSubsystem;
+  }
+
+  // Define routine
+  AutoRoutine routine = super.getAutoFactory().newRoutine("B_RightTrenchMoveFirstAuto");
+
+  // Load trajectories
+  AutoTrajectory blueRightNeutralZone = routine.trajectory("BlueRightNeutralZone");
+  AutoTrajectory blueRightTransitionToNZ = routine.trajectory("BlueRightTransitionToNZ");
+
+  @Override
+  public String getName() {
+    return "BlueRightTrenchMoveFirstAuto";
+  }
+
+  @Override
+  public AutoTrajectory getInitialTrajectory() {
+    return blueRightNeutralZone;
+  }
+
+  @Override
+  public AutoRoutine getAutoRoutine() {
+
+    routine
+        .active()
+        .onTrue(
+            Commands.sequence(
+                blueRightNeutralZone.resetOdometry(),
+                Commands.parallel(
+                    blueRightNeutralZone.cmd(), intake.getDeployCommand().withTimeout(10.0))));
+
+    blueRightNeutralZone
+        .done()
+        .onTrue(
+            Commands.sequence(
+                Commands.runOnce(drive::stop, drive),
+                Commands.startEnd(feeder::spinForward, () -> {}, feeder).withTimeout(5.0),
+                Commands.parallel(
+                    blueRightTransitionToNZ.cmd(), intake.getDeployCommand().withTimeout(5.0))));
+
+    blueRightTransitionToNZ
+        .done()
+        .onTrue(
+            Commands.sequence(
+                Commands.runOnce(drive::stop, drive),
+                Commands.startEnd(feeder::spinForward, () -> {}, feeder).withTimeout(5.0)));
+
+    return routine;
+  }
+}
